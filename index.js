06a9c0a92edcf70c6ce488a29b09d447d7fe678c
GauravMishraaaa <gauravm844@gmail.com>
2024-07-21 19:37:05 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var bt = document.getElementById("b");
-
-bt.addEventListener("click", function() {
-    window.location.href='checkout.html';
-    bt.style.backgroundColor = "green";
-    bt.disabled= true;
-});
-
